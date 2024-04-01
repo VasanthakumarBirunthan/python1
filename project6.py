38ea8be731cb1ebeed2e89f9d98821af29b59d7b
VasanthakumarBirunthan <165446227+VasanthakumarBirunthan@users.noreply.github.com>
2024-04-01 19:34:08 +0530

    Create project6.py

---
+++ project6.py
@@ -0,0 +1,13 @@
+res=int(input("enter no of inputs u want toadd:=   "))
+tsum=0
+for i in range(res):
+    sum=int(input("Enter a num:=  "))
+    tsum=tsum+sum
+print(tsum)
+
+
+""""Write a program that gets a dynamic number of input values.
+
+The first input is a number that represents the number of the input values following it. The next input values are whole numbers.
+
+In the end, print the sum of all the input numbers (not including the first input). """
