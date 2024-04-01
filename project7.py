5f83a8f5a1f4c2bd45d843e9c09d95e307dadd68
VasanthakumarBirunthan <165446227+VasanthakumarBirunthan@users.noreply.github.com>
2024-04-01 19:35:34 +0530

    Create project7.py

---
+++ project7.py
@@ -0,0 +1,10 @@
+#count_1=int(input("enter how many times this wiil occur:::   "))
+
+
+def sumof1000 ():
+    sum_1=0
+    for i in range(1001):
+      sum_1 =sum_1+ i
+    print(sum_1)    
+sumof1000()  
+sumof1000()
